ed285c68a1d5f07c5a79e3226b3abf01048bc297
Sudhakar <sudhakarkr21@gmail.com>
2022-02-01 18:16:19 +0530

    Testing

--- app/src/main/java/com/example/printerdemo/MainActivity.java
+++ app/src/main/java/com/example/printerdemo/MainActivity.java
@@ -42,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
     CieBluetoothPrinter cieBluetoothPrinter = new CieBluetoothPrinter();
     Bitmap mergerBitMap = null;
     ImageView image;
+    int test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
